a9287b4ce7f251afe90e017349c77c82d55358f4
n.savelev <n.savelev@bi.zone>
2024-02-18 22:29:10 +0300

    cd exit no pipes

--- 2/solution.c
+++ 2/solution.c
@@ -2,7 +2,87 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+
+static void 
+execute_cd(const struct command *cmd) {
+	assert(cmd != NULL);
+	assert(cmd->exe != NULL);
+
+	if (strcmp(cmd->exe, "cd") == 0) {
+		if (cmd->arg_count == 1) { // 1 argument, valid
+			if (chdir(cmd->args[0]) == 0) {
+				printf("Changed directory to: %s\n", cmd->args[0]);
+			}
+			else {
+				if (errno == ENOENT) { // no such file or dir
+					fprintf(stderr, "cd: no such file or directory: %s\n", cmd->args[0]);
+				}
+				else {
+					perror("chdir");
+				}
+			}
+		}
+		else if (cmd->arg_count > 1) { // > 1 args, invalid
+			fprintf(stderr, "cd: too many arguments\n");
+		}
+		else if (cmd->arg_count == 0) { // switch to home dir
+			if (chdir(getenv("HOME")) == 0) {
+				printf("Changed directory to home\n");
+			} 
+			else {
+				perror("chdir");
+			}
+		}
+		else {
+			fprintf(stderr, "cd: missing arguments\n");
+		}
+	}
+}
+
+static void 
+execute_exit(const struct command *cmd) {
+	assert(cmd != NULL);
+	assert(cmd->exe != NULL);
+
+	if (strcmp(cmd->exe, "exit") == 0) {
+		if (cmd->arg_count > 1) {
+			fprintf(stderr, "exit: too many arguments\n");
+		}
+		else {
+			exit(EXIT_SUCCESS);
+		}
+	}
+}
+
+static void
+execute_command(const struct command *cmd) {
+	pid_t pid = fork();
+
+	if (pid == -1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0) {
+		if (execvp(cmd->exe, cmd->args) == -1) {
+			perror("execvp");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else {
+		int status;
+		if (waitpid(pid, &status, 0) == -1) {
+			perror("waitpid");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
 
 static void
 execute_command_line(const struct command_line *line)
@@ -10,6 +90,20 @@ execute_command_line(const struct command_line *line)
 	/* REPLACE THIS CODE WITH ACTUAL COMMAND EXECUTION */
 
 	assert(line != NULL);
+
+	if (line->head != NULL && line->head->type == EXPR_TYPE_COMMAND && 
+		strcmp(line->head->cmd.exe, "cd") == 0 && line->head->next == NULL) {
+			execute_cd(&line->head->cmd);
+			return;
+	}
+
+	if (line->head != NULL && line->head->type == EXPR_TYPE_COMMAND && 
+		strcmp(line->head->cmd.exe, "exit") == 0 && line->head->next == NULL) {
+			execute_exit(&line->head->cmd);
+			return;
+	}
+
+/*
 	printf("================================\n");
 	printf("Command line:\n");
 	printf("Is background: %d\n", (int)line->is_background);
@@ -24,12 +118,12 @@ execute_command_line(const struct command_line *line)
 		assert(false);
 	}
 	printf("Expressions:\n");
+*/
 	const struct expr *e = line->head;
 	while (e != NULL) {
 		if (e->type == EXPR_TYPE_COMMAND) {
 			printf("\tCommand: %s", e->cmd.exe);
-			for (uint32_t i = 0; i < e->cmd.arg_count; ++i)
-				printf(" %s", e->cmd.args[i]);
+			execute_command(&e->cmd);
 			printf("\n");
 		} else if (e->type == EXPR_TYPE_PIPE) {
 			printf("\tPIPE\n");
